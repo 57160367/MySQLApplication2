b3f64481094019d6f3340201309a144cfaa51a0a
Nutty <se57160367@gmail,com>
2017-03-16 22:47:20 +0700

    Initial commit 2

---
+++ app/src/main/java/com/example/nutty/mysqlapplication/EditActivity.java
@@ -0,0 +1,120 @@
+package com.example.nutty.mysqlapplication;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class EditActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        final TodoList editTodoList = (TodoList) getIntent().getSerializableExtra("editTodoList");
+
+        final EditText editText = (EditText) findViewById(R.id.edit_editText);
+        editText.setText(editTodoList.getTaskname());
+
+        Button editBtn = (Button) findViewById(R.id.edit_button);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TodoList eTodoList = new TodoList();
+                eTodoList.setTaskid(editTodoList.getTaskid());//ดึงค่าจาก edittext มาใส่ในนี้
+                eTodoList.setTaskname(String.valueOf(editText.getText()));
+
+                TodoListDAO todoListDAO = new TodoListDAO(getApplicationContext());
+                todoListDAO.open();
+                todoListDAO.update(eTodoList);
+                todoListDAO.close();
+
+                finish(); //กลับมาหน้าจอเดิม
+            }
+        });
+
+        Button delBtn = (Button) findViewById(R.id.delete_btn);
+        delBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TodoListDAO todoListDAODel = new TodoListDAO(getApplicationContext()
+                );
+                todoListDAODel.open();
+                todoListDAODel.delete(editTodoList);
+                todoListDAODel.close();
+
+                finish();
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add_new_menu) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Edit Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
